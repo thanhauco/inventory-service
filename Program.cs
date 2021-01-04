c43d769894f1306b9aced0fec6f1be2a4fe4345b
Thanh Vu <thanhauco@gmail.com>
2021-01-04 13:08:00 -0800

    Initial project structure

---
+++ Program.cs
@@ -0,0 +1,3 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+namespace InventoryService { public class Program { public static void Main(string[] args) { CreateHostBuilder(args).Build().Run(); } public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); }); } }
