c8825c7345f283c72f4de417366992b54c52f238
Thanh Vu <thanhauco@gmail.com>
2021-01-09 08:16:00 -0800

    Add properties to Item model

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,1 +1,1 @@
-namespace InventoryService.Models { public class Item { public int Id { get; set; } public string Name { get; set; } } }
+namespace InventoryService.Models { public class Item { public int Id { get; set; } public string Name { get; set; } public decimal Price { get; set; } public int Stock { get; set; } } }
