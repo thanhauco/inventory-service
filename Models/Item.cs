e2086ab66bd4ede7bbcda9f2d02f260b78338aad
Thanh Vu <thanhauco@gmail.com>
2021-01-11 01:06:00 -0800

    Add Category to Item

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,1 +1,1 @@
-namespace InventoryService.Models { public class Item { public int Id { get; set; } public string Name { get; set; } public decimal Price { get; set; } public int Stock { get; set; } } }
+namespace InventoryService.Models { public class Item { public int Id { get; set; } public string Name { get; set; } public decimal Price { get; set; } public int Stock { get; set; } public string Category { get; set; } } }
