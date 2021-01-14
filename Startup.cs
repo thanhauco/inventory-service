73f155be9f242a187ff66322b98fb078734ffc46
Thanh Vu <thanhauco@gmail.com>
2021-01-14 14:17:00 -0800

    Register DbContext

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,5 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
-namespace InventoryService { public class Startup { public void ConfigureServices(IServiceCollection services) { services.AddControllers(); } public void Configure(IApplicationBuilder app) { app.UseRouting(); app.UseEndpoints(e => e.MapControllers()); } } }
+using Microsoft.EntityFrameworkCore;
+using InventoryService.Data;
+namespace InventoryService { public class Startup { public void ConfigureServices(IServiceCollection services) { services.AddDbContext<InventoryContext>(o => o.UseInMemoryDatabase("Inv")); services.AddControllers(); } public void Configure(IApplicationBuilder app) { app.UseRouting(); app.UseEndpoints(e => e.MapControllers()); } } }
