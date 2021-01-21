d8c32573889428a68c7d4856312a1251e5707cb1
Thanh Vu <thanhauco@gmail.com>
2021-01-20 18:12:00 -0800

    Add GetById endpoint

--- Controllers/ItemsController.cs
+++ Controllers/ItemsController.cs
@@ -1,4 +1,4 @@
 using Microsoft.AspNetCore.Mvc;
 using InventoryService.Data;
 using System.Linq;
-namespace InventoryService.Controllers { [ApiController] [Route("[controller]")] public class ItemsController : ControllerBase { private readonly InventoryContext _ctx; public ItemsController(InventoryContext ctx) { _ctx = ctx; } [HttpGet] public IActionResult Get() => Ok(_ctx.Items.ToList()); } }
+namespace InventoryService.Controllers { [ApiController] [Route("[controller]")] public class ItemsController : ControllerBase { private readonly InventoryContext _ctx; public ItemsController(InventoryContext ctx) { _ctx = ctx; } [HttpGet] public IActionResult Get() => Ok(_ctx.Items.ToList()); [HttpGet("{id}")] public IActionResult Get(int id) { var i = _ctx.Items.Find(id); if(i==null) return NotFound(); return Ok(i); } } }
