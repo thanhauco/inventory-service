b4ef34f30bec8ddf3f290e4f3226e4e81be2a8f7
Thanh Vu <thanhauco@gmail.com>
2021-02-06 02:49:00 -0800

    Register ItemService

--- Startup.cs
+++ Startup.cs
@@ -2,4 +2,5 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using InventoryService.Data;
-namespace InventoryService { public class Startup { public void ConfigureServices(IServiceCollection services) { services.AddDbContext<InventoryContext>(o => o.UseInMemoryDatabase("Inv")); services.AddControllers(); } public void Configure(IApplicationBuilder app) { app.UseRouting(); app.UseEndpoints(e => e.MapControllers()); } } }
+using InventoryService.Services;
+namespace InventoryService { public class Startup { public void ConfigureServices(IServiceCollection services) { services.AddDbContext<InventoryContext>(o => o.UseInMemoryDatabase("Inv")); services.AddScoped<IItemService, ItemService>(); services.AddControllers(); } public void Configure(IApplicationBuilder app) { app.UseRouting(); app.UseEndpoints(e => e.MapControllers()); } } }
