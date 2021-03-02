f8edfe0dfebf5aa7df7c6822d82a0aef91690c8f
Thanh Vu <thanhauco@gmail.com>
2021-03-01 21:58:00 -0800

    Cleanup Program.cs

--- Program.cs
+++ Program.cs
@@ -2,4 +2,4 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using InventoryService.Data;
-namespace InventoryService { public class Program { public static void Main(string[] args) { var host = CreateHostBuilder(args).Build(); using(var scope = host.Services.CreateScope()) { DbSeeder.Seed(scope.ServiceProvider.GetRequiredService<InventoryContext>()); } host.Run(); } public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); }); } }
+namespace InventoryService { public class Program { public static void Main(string[] args) { var host = CreateHostBuilder(args).Build(); using(var scope = host.Services.CreateScope()) { DbSeeder.Seed(scope.ServiceProvider.GetRequiredService<InventoryContext>()); } host.Run(); } public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); }); } // Cleaned up
