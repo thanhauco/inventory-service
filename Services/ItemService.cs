197e9593f79a48574b5d1086d60cc3edebf26079
Thanh Vu <thanhauco@gmail.com>
2021-03-07 12:59:00 -0800

    Use Cache in Service

--- Services/ItemService.cs
+++ Services/ItemService.cs
@@ -2,4 +2,4 @@ using System.Collections.Generic;
 using System.Linq;
 using InventoryService.Data;
 using InventoryService.Models;
-namespace InventoryService.Services { public class ItemService : IItemService { private readonly InventoryContext _ctx; public ItemService(InventoryContext ctx) { _ctx = ctx; } public IEnumerable<Item> GetAll() => _ctx.Items.ToList(); public Item Get(int id) => _ctx.Items.Find(id); public void Add(Item i) { _ctx.Items.Add(i); _ctx.SaveChanges(); } } }
+namespace InventoryService.Services { public class ItemService : IItemService { private readonly InventoryContext _ctx; private readonly CacheService _cache = new CacheService(); public ItemService(InventoryContext ctx) { _ctx = ctx; } public IEnumerable<Item> GetAll() => _ctx.Items.ToList(); public Item Get(int id) { var c = _cache.Get(id.ToString()); if(c!=null) return (Item)c; var i = _ctx.Items.Find(id); if(i!=null) _cache.Set(id.ToString(), i); return i; } public void Add(Item i) { _ctx.Items.Add(i); _ctx.SaveChanges(); } } }
