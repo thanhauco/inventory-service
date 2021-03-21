229162df6d8273d48f4332e74e2e1a71f2de102f
Thanh Vu <thanhauco@gmail.com>
2021-03-21 10:23:00 -0700

    Add Serilog

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using InventoryService.Data;
-namespace InventoryService { public class Program { public static void Main(string[] args) { var host = CreateHostBuilder(args).Build(); using(var scope = host.Services.CreateScope()) { DbSeeder.Seed(scope.ServiceProvider.GetRequiredService<InventoryContext>()); } host.Run(); } public static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); }); } // Cleaned up
+using Serilog;
+public class Program {
+    public static void Main(string[] args) {
+        Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+        CreateHostBuilder(args).Build().Run();
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args).UseSerilog().ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+}
