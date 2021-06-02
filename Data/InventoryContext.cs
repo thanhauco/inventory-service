d9d26b0bd85434f52d1c685b3005204658ab30ea
Thanh Vu <thanhauco@gmail.com>
2021-06-01 23:27:00 -0700

    Add Database Index

--- Data/InventoryContext.cs
+++ Data/InventoryContext.cs
@@ -1,3 +1,10 @@
 using Microsoft.EntityFrameworkCore;
-using InventoryService.Models;
-namespace InventoryService.Data { public class InventoryContext : DbContext { public InventoryContext(DbContextOptions<InventoryContext> options) : base(options) { } public DbSet<Item> Items { get; set; } } }
+// ... imports
+namespace InventoryService.Data {
+    public class InventoryContext : DbContext {
+        // ... existing
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Item>().HasIndex(i => i.Name);
+        }
+    }
+}
