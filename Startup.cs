aaeeb98a77e4d8ce0bb53621b24b85f7949af3e1
Thanh Vu <thanhauco@gmail.com>
2021-06-04 14:47:00 -0700

    Register Security Headers

--- Startup.cs
+++ Startup.cs
@@ -1,14 +1,8 @@
-using HotChocolate;
+using InventoryService.Middleware;
 // ... imports
 public class Startup {
-    public void ConfigureServices(IServiceCollection services) {
-        services.AddGraphQLServer().AddQueryType<Query>();
-        // ... rest
-    }
     public void Configure(IApplicationBuilder app) {
-        app.UseEndpoints(e => {
-            e.MapGraphQL();
-            e.MapControllers();
-        });
+        app.UseMiddleware<SecurityHeaders>();
+        // ... rest
     }
 }
