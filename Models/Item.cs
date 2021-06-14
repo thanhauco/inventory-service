ef8da34eb79f1839da7b3b9dc8e60ac582b79d70
Thanh Vu <thanhauco@gmail.com>
2021-06-14 06:43:00 -0700

    Refactor Item Model

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,1 +1,13 @@
-namespace InventoryService.Models { public class Item { public int Id { get; set; } public string Name { get; set; } public decimal Price { get; set; } public int Stock { get; set; } public string Category { get; set; } } }
+using System.ComponentModel.DataAnnotations;
+namespace InventoryService.Models {
+    public class Item {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+        [Range(0, 10000)]
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Category { get; set; }
+    }
+}
