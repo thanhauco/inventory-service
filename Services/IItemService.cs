ed57dff86d01e1d8014dd0c5e17c8e0625559036
Thanh Vu <thanhauco@gmail.com>
2021-06-30 08:22:00 -0700

    Add Async Suffix

--- Services/IItemService.cs
+++ Services/IItemService.cs
@@ -1,3 +1,9 @@
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using InventoryService.Models;
-namespace InventoryService.Services { public interface IItemService { IEnumerable<Item> GetAll(); Item Get(int id); void Add(Item i); } }
+namespace InventoryService.Services {
+    public interface IItemService {
+        Task<IEnumerable<Item>> GetAllAsync();
+        // ... rest
+    }
+}
