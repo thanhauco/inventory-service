323e2fcc8fe2182d145e1a178ca294443d4e22d9
Thanh Vu <thanhauco@gmail.com>
2022-01-06 06:57:00 -0800

    File-scoped Namespaces

--- Models/Item.cs
+++ Models/Item.cs
@@ -1,13 +1,12 @@
 using System.ComponentModel.DataAnnotations;
-namespace InventoryService.Models {
-    public class Item {
-        public int Id { get; set; }
-        [Required]
-        [MaxLength(100)]
-        public string Name { get; set; }
-        [Range(0, 10000)]
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public string Category { get; set; }
-    }
+namespace InventoryService.Models;
+public class Item {
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public string Name { get; set; }
+    [Range(0, 10000)]
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+    public string Category { get; set; }
 }
