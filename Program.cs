b8492994d3d7331be744ade5706d9d917cb7fd81
Thanh Vu <thanhauco@gmail.com>
2022-01-18 22:36:00 -0800

    Minimal APIs Refactor

--- Program.cs
+++ Program.cs
@@ -1,10 +1,13 @@
-using Serilog;
-public class Program {
-    public static void Main(string[] args) {
-        Log.Logger = new LoggerConfiguration()
-            .WriteTo.Console()
-            .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day)
-            .CreateLogger();
-        // ... rest
-    }
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+var app = builder.Build();
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+app.UseAuthorization();
+app.MapControllers();
+app.MapGet("/minimal", () => "Hello Minimal API");
+app.Run();
